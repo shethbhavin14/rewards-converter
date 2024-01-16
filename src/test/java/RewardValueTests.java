bf25e21a2c95f4678624fbc49ce918bce3e2f112
Bhavin Sheth <shethbhavin14@gmail.com>
2024-01-16 21:20:38 +0530

    Test Functions for RewardValueTests
    Tested functions for RewardValueTests and verified the code to see the conversion rate is accurate.

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,7 +3,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+    
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +21,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
